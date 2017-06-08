f0e21a85c0f48396960a98ccabdce5a166957ff0
Gaoyagi <aoyagigeorge@gmail.com>
2017-06-08 14:55:20 -0700

    Update Report.h
    again this is the header that we made last week altho i added the int amount;

--- Report.h
+++ Report.h
@@ -1,1 +1,29 @@
 
+#define Report_H
+
+#include <string>
+#include <fstream>
+
+class Report
+{
+	struct Inven
+	{
+		std::string isbn, title, author, publisher, date, quantity;
+		double wholeC, retailP;
+	};
+	double wholeV, retailV;					//all of whole sale and all of retail
+	int amount;								//the total number of books
+public:
+	Report();
+	void getList();
+	void setWholeSaleValue();
+	double getWholeSaleValue();
+	void setRetailValue();
+	double getRtailValue();
+	void sortQuan();
+	void sortCost();
+	void sortAge();
+	~Report();
+};
+
+#endif
