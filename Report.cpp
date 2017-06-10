718c58d1abe10154e13c60224c97ebe0a35a2ac6
Gaoyagi <aoyagigeorge@gmail.com>
2017-06-10 14:57:21 -0700

    Create Report.cpp

--- Report.cpp
+++ Report.cpp
@@ -3,89 +3,205 @@
 #include <fstream>
 #include <string>
 
-Inven sample[10];	//10 will later bechanged to amount variable
-std::ifstream test("test.txt");
+std::ifstream test;
+
+
 Report::Report()
 {
-	test >> sample.isbn >> sample.title >> sample.author >> sample.publisher >> sample.date >> sample.quantity >> sample.wholeC >> sample.retailP;
-	//parsing the file and initalizing the structs variables to the file's values
+	test.open("test.txt");
+
+	std::string stfd;
+	getline(test, stfd);
+	for (int g = 0; g < 25; g++)
+	{
+		test >> sample[g].isbn >> sample[g].title >> sample[g].author >> sample[g].publisher >> sample[g].date >> sample[g].quantity >> sample[g].wholeC >> sample[g].retailP;
+		//parsing the file and initalizing the structs variables to the file's values
+	}	
 }
 
-void Report::sort(int a[], int size)					//next 2 is the selection sort we need for the getQuan,Cost, Age
+void Report::getList()		//returns the full list of book details
 {
-	int minIndex;
-	for (int i = 0; i < size - 1; i++)
+	std::cout << "ISBN\tTitle\tAuthor	Publisher	Date		Quantity	Whole Sale Cost	Retail Price" << std::endl;
+	for (int x = 0; x < 25; x++)
 	{
-		minIndex = i;
-		for (int j = i + 1; j < size; j++)
-		{
-			if (a[j] < a[minIndex])
-			{
-				minIndex = j;
-			}
-		}
-		if (minIndex != i)
-		{
-			swap(a[i], a[minIndex]);
-		}
-		std::cout << "$" << minIndex << endl;
+		std::cout << sample[x].isbn << "	" << sample[x].title << "	" << sample[x].author << "	" << sample[x].publisher << "		" 
+			<< sample[x].date << "	" << sample[x].quantity << "		" << sample[x].wholeC << "		" << sample[x].retailP << "		" << std::endl;
 	}
 }
-
-void Report::swap(int& a, int& b)
+void Report::setWholeSaleValue()		//adds the value of all the whole sale costs added together
 {
-	int temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
-
-
-void Report::getList()
-{
-	for (int x = 0; x < 10; x++)
+	wholeV = 0;
+	for (int y = 0; y < 25; y++)
 	{
-		std::cout << sample.isbn << sample.title << sample.author << sample.publisher << sample.date << sample.quantity << sample.wholeC << sample.retailP;
-	}
-}
-void Report::setWholeSaleValue()
-{
-	for (int y = 0; y < 10; y++)
-	{
+		std::cout << sample[y].title << "	" << "$" << sample[y].wholeC << std::endl;
 		wholeV += sample[y].wholeC;
 	}
 }
-double Report::getWholeSaleValue()
+double Report::getWholeSaleValue()		// returns a list of all the whole sale cost
 {
+	setWholeSaleValue();
+	std::cout << "the total wholesale value is $";
 	return wholeV;
 }
-void Report::setRetailValue()
+void Report::setRetailValue()			//returns the value of all the retail values added up
 {
-	for (int y = 0; y < 10; y++)
+	for (int y = 0; y < 25; y++)
 	{
-		retailV += sample[y].retailC;
+		std::cout << sample[y].title << "	" << "$" << sample[y].retailP << std::endl;
+		retailV += sample[y].retailP;
 	}
 }
-double Report::getRtailValue()
+double Report::getRetailValue()		//returns a list of all the retail values
 {
+	setRetailValue();
+	std::cout << "the total retail value is $";
 	return retailV;
 }
-void Report::sortQuan()
+void Report::sortQuan()		//prints out the list of all the sorted quantitys
 {
-	sort(sample[].quantity, 10);
+	Inven alpha[25];
+	for (int g = 0; g < 25; g++)
+	{
+		alpha[g] = sample[g];
+	}
+	sortByQuan(alpha, 25);
+	for (int i = 0; i < 25; i++)
+	{
+		std::cout << alpha[i].quantity << " of " << alpha[i].title << std::endl;
+	}
+}
 
-}
-void Report::sortCost()
+void Report::sortByQuan(Inven array[], int max)			//sorts all the books by quantitys
 {
-	sort(sample[].Cost, 10);
+	for (int x = 0; x < max; x++)
+	{
+	
+		double min = array[x].quantity;
+		int index = x;
+		for (int y = x; y < max; y++)
+		{
+			if (array[y].quantity < min)
+			{
+				min = array[y].quantity;
+				index = y;
+			}
+		}
+		Inven temp = array[x];
+		array[x] = array[index];
+		array[index] = temp;
+	}
 }
-void Report::sortAge()
+
+void Report::sortCost()							//prints out all the sorted whole sale prices
 {
-	sort(sample[].Age, 10);
+	Inven beta[25];
+	for (int g = 0; g < 25; g++)
+	{
+		beta[g] = sample[g];
+	}
+	sortByPrice(beta, 25);
+	for (int i = 0; i < 25; i++)
+	{
+		std::cout << beta[i].title << " $" <<beta[i].wholeC << std::endl;
+	}
+}
+void Report::sortByPrice(Inven array[], int max)		//sorts all the books by whole sale price
+{
+	for (int x = 0; x < max; x++)
+	{
+		double min = array[x].wholeC;
+		int index = x;
+		for (int y = x; y < max; y++)
+		{
+			if (array[y].wholeC < min)
+			{
+				min = array[y].wholeC;
+				index = y;
+			}
+		}
+		Inven temp = array[x];
+		array[x] = array[index];
+		array[index] = temp;
+	}
+}
+
+int Report::compareDate(std::string a, std::string b)		//compares 2 years then if the 2nd is bigger than the 1st it compares the 2 months, if the 2nd month is bigger than the first, it compares the 2 days
+{
+	std::string aYear = a.substr(7, 4);						//mm/dd/yyyy
+	std::string aMonth = a.substr(2);
+	std::string aDay = a.substr(4, 2);
+	std::string bYear = b.substr(7, 4);
+	std::string bMonth = b.substr(2);
+	std::string bDay = b.substr(4, 2);
+	if (aYear < bYear)
+	{
+		return 1;
+	}
+	else if (bYear < aYear)
+	{
+		return -1;
+	}
+	else
+	{
+		if (aMonth < bMonth)
+		{
+			return 1;
+		}
+		else if (bMonth < aMonth)
+		{
+			return -1;
+		}
+		else
+		{
+			if (aDay < bDay)
+			{
+				return 1;
+			}
+			else if (bDay < aDay)
+			{
+				return -1;
+			}
+		}
+	}
+	return 0;
+}
+
+
+void Report::sortByDate(Inven array[], int max)		//sorts the dates using the compareDate
+{
+	for (int x = 0; x < max; x++)
+	{
+		std::string curr = array[x].date;
+		int index = x; 
+		for (int y = x+1; y < max; y++)
+		{
+			if(compareDate(curr, array[y].date) == -1)
+			{
+				curr = array[y].date;
+				index = y;
+			}
+		}
+		Inven temp = array[x];
+		array[x] = array[index];
+		array[index] = temp;
+	}
+}
+void Report::sortAge()			//prints out all the sorted books by age in inventory
+{
+	Inven omega[25];
+	for (int g = 0; g < 25; g++)
+	{
+		omega[g] = sample[g];
+	}
+	sortByDate(omega, 25);
+	for (int i = 0; i < 25; i++)
+	{
+		std::cout << omega[i].title << " purchased on " << omega[i].date << std::endl;
+	}
+
 }
 
 
 Report::~Report()
 {
 }
-
